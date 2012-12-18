dec58ddc934c37b15c7168a7bfdea532ed428ba4
Trong Tran <trongrg@gmail.com>
2012-12-19 00:21:53 +1100

    add meta search scroll

---
+++ scrolls/meta_search.rb
@@ -0,0 +1,9 @@
+gem 'meta_search'
+
+__END__
+
+name: Meta search
+description:
+website:
+author: trongtran
+category: other # authentication, testing, persistence, javascript, css, services, deployment, and templating
