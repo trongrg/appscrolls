7e6a6ba65fa4f7f86e131d9db58913c3c8de06cd
Trong Tran <trongrg@gmail.com>
2012-12-19 00:22:07 +1100

    add squeel scroll

---
+++ scrolls/squeel.rb
@@ -0,0 +1,14 @@
+gem 'squeel'
+
+after_bundler do
+  generate 'squeel:initializer'
+end
+
+__END__
+
+name: Squeel
+description:
+website:
+author: trongtran
+
+category: other # authentication, testing, persistence, javascript, css, services, deployment, and templating
