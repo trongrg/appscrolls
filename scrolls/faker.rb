64b4e9255484ee9ee14d2776fe1b0fb6c985bac1
Trong Tran <trongrg@gmail.com>
2012-12-19 00:21:28 +1100

    add faker scroll

---
+++ scrolls/faker.rb
@@ -0,0 +1,10 @@
+gem 'faker', :group => :test
+
+__END__
+
+name: Faker
+description:
+website:
+author: trongtran
+
+category: other # authentication, testing, persistence, javascript, css, services, deployment, and templating
