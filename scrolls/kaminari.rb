b4d1c498a8876d0cc5caf14a662328ba8e53819b
Trong Tran <trongrg@gmail.com>
2012-12-19 00:21:35 +1100

    add kaminari scroll

---
+++ scrolls/kaminari.rb
@@ -0,0 +1,10 @@
+gem 'kaminari'
+
+__END__
+
+name: Kaminari
+description:
+website:
+author: trongtran
+
+category: templating # authentication, testing, persistence, javascript, css, services, deployment, and templating
