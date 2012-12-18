af37282c56997bcf321f95dea28147ff1559134c
Trong Tran <trongrg@gmail.com>
2012-12-19 00:21:59 +1100

    add pry scroll

---
+++ scrolls/pry.rb
@@ -0,0 +1,12 @@
+gem 'pry-rails', :group => :development
+gem 'pry-nav', :group => :development
+gem 'awesome_print', :group => :development
+
+__END__
+
+name: Pry
+description:
+website:
+author: trongtran
+
+category: other # authentication, testing, persistence, javascript, css, services, deployment, and templating
