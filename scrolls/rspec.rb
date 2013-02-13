575c2e5515dd1d0f31a0cb42c8687823a74abfac
Trong Tran <trongrg@gmail.com>
2013-02-13 17:29:44 +1100

    use rspec-rails ~> 2.0

--- scrolls/rspec.rb
+++ scrolls/rspec.rb
@@ -1,4 +1,4 @@
-gem 'rspec-rails', :group => [:development, :test]
+gem 'rspec-rails', '~> 2.0', :group => [:development, :test]
 
 inject_into_file "config/initializers/generators.rb", :after => "Rails.application.config.generators do |g|\n" do
   "    g.test_framework = :rspec\n"
