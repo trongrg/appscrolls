1d324c37c2f26e194287d999d06df060a8b69f9f
Trong Tran <trongrg@gmail.com>
2013-02-15 00:35:27 +1100

    update rvm scroll

--- scrolls/rvm.rb
+++ scrolls/rvm.rb
@@ -1,14 +1,15 @@
 before_everything do
   gemset = config['gemset'].present? ? config['gemset'] : app_name
   create_file '.rvmrc', <<-END
-  rvm use 2.0.0@#{gemset} --create
-  END
+rvm use 2.0.0@#{gemset} --create
+END
 
   run 'rvm rvmrc trust .'
   run 'source .rvmrc'
 end
 
 __END__
+
 name: RVM
 description: Creates .rvmrc file and gemset
 category: other
