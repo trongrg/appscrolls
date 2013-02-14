d732fbbfc7aafb2feb65a9f8ec5d9702e985872d
Trong Tran <trongrg@gmail.com>
2013-02-15 02:36:07 +1100

    improve compass scroll
    - import compass in application.css.scss
    - run after sass scroll

--- scrolls/compass.rb
+++ scrolls/compass.rb
@@ -1,11 +1,19 @@
 gem 'compass-rails', :group => :assets
 
+after_bundler do
+  append_file "app/assets/stylesheets/application.css.scss", <<-SCSS
+// Import compass
+@import "compass";
+SCSS
+end
+
 __END__
 
 name: Compass
 description: "Use Compass Stylesheet Authoring Framework for Ruby on Rails."
 
 requires: [sass]
+run_after: [sass]
 
 category: assets
 tags: [css, stylesheet]
