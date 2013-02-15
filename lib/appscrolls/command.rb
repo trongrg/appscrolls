312baab03a0ff459259c0e2c1231d13233f4a269
Trong Tran <trongrg@gmail.com>
2013-02-15 22:24:22 +1100

    define scrolls add command to add more scrolls to existing app

--- lib/appscrolls/command.rb
+++ lib/appscrolls/command.rb
@@ -52,6 +52,38 @@ module AppScrolls
       end
     end
 
+    desc "add scrolls", "add more scrolls to existing Rails app"
+    method_option :scrolls, :type => :array, :aliases => "-s", :desc => "List scrolls, e.g. -s resque rails_basics jquery"
+    def add
+      unless File.read("Gemfile").include?("rails")
+        puts "Not a Rails application, exit"
+        return
+      end
+      app_name = Dir.pwd.split('/').last
+      scrolls = options[:scrolls]
+      puts
+      puts
+      puts "#{bold}Generating and Running Template...#{clear}"
+      puts
+
+      template = create_template(scrolls)
+      puts
+
+      file_name = "lib/generators/app_scrolls/app_scrolls_generator.rb"
+      FileUtils.mkdir_p(File.dirname(file_name))
+      file = File.open(file_name, 'w')
+      file.write template.compile
+      file.close
+      content = <<-RUBY
+class AppScrollsGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("../templates", __FILE__)
+  def add
+RUBY
+      inject_into_file file_name, content, :after => "# END MODULE\n"
+      append_file file_name, "\nend\nend"
+      run "rails g app_scrolls add"
+    end
+
     no_tasks do
       def cyan; "\033[36m" end
       def clear; "\033[0m" end
@@ -80,17 +112,8 @@ module AppScrolls
         puts "#{bold}Generating and Running Template...#{clear}"
         puts
         file = Tempfile.new('template')
-        template_options = {}
-        template_options[:config_script] = File.read(options[:config_file]) if options[:config_file]
-        template = AppScrolls::Template.new(scrolls)
-
-        puts "Using the following scrolls:"
-        template.resolve_scrolls.map do |scroll|
-          color = scrolls.include?(scroll.new.key) ? green : yellow # yellow - automatic dependency
-          puts "  #{color}* #{scroll.new.name}#{clear}"
-        end
         puts
-
+        template = create_template(scrolls)
         file.write template.compile
         file.close
         if display_only
@@ -101,6 +124,19 @@ module AppScrolls
         end
       ensure
         file.unlink
+      end
+
+      def create_template(scrolls)
+        template_options = {}
+        template_options[:config_script] = File.read(options[:config_file]) if options[:config_file]
+        template = AppScrolls::Template.new(scrolls)
+
+        puts "Using the following scrolls:"
+        template.resolve_scrolls.map do |scroll|
+          color = scrolls.include?(scroll.new.key) ? green : yellow # yellow - automatic dependency
+          puts "  #{color}* #{scroll.new.name}#{clear}"
+        end
+        template
       end
     end
   end
