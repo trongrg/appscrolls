110aae63ce5ad4768da8146f0f4a5447e1bf6354
Trong Tran <trongrg@gmail.com>
2013-02-15 18:31:36 +1100

    use rspec-instafail in rspec scroll

--- scrolls/rspec.rb
+++ scrolls/rspec.rb
@@ -1,11 +1,19 @@
 gem 'rspec-rails', '~> 2.0', :group => [:development, :test]
-
-inject_into_file "config/initializers/generators.rb", :after => "Rails.application.config.generators do |g|\n" do
-  "    g.test_framework = :rspec\n"
-end
+gem 'rspec-instafail', :group => :test
 
 after_bundler do
   generate 'rspec:install'
+end
+
+after_everything do
+  inject_into_file "config/initializers/generators.rb", :after => "Rails.application.config.generators do |g|\n" do
+    "    g.test_framework = :rspec\n"
+  end
+
+  append_file '.rspec', <<-RSPEC
+--require rspec/instafail
+--format RSpec::Instafail
+RSPEC
 end
 
 __END__
