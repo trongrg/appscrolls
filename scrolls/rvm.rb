c49203c85f22f4b6e6453e2f4870a5ffdd37a971
Trong Tran <trongrg@gmail.com>
2013-03-20 17:31:43 +1100

    update rvm scrolls: use .ruby-version and .ruby-gemset instead of .rvmrc

--- scrolls/rvm.rb
+++ scrolls/rvm.rb
@@ -1,8 +1,11 @@
 before_everything do
   gemset = config['gemset'].present? ? config['gemset'] : app_name
-  create_file '.rvmrc', <<-END
-rvm use 2.0.0@#{gemset} --create
+  ruby_version = config['ruby_version'].present? ? config['ruby_version'] : '2.0.0'
+  create_file '.ruby-version', <<-END
+ruby-#{ruby_version}
 END
+
+  create_file '.ruby-gemset', gemset
 
   run 'rvm rvmrc trust .'
   run 'source .rvmrc'
@@ -15,5 +18,8 @@ description: Creates .rvmrc file and gemset
 category: other
 config:
   - gemset:
-      prompt: "Specify gemset for RVM file:"
+      prompt: "Specify gemset for RVM:"
+      type: string
+  - ruby_version:
+      prompt: "Specify ruby version for RVM (default to 2.0.0):"
       type: string
